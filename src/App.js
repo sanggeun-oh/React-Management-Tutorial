cbb5c3311cdb8dc4b59d4770ff85faa031636bf8
sanggeun-oh <rjfjqj04@naver.com>
2021-02-23 11:31:57 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React,{Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const styles= {
+  root:{
+    width:'100%',
+    
+    overflowX:"auto"
+
+  },
+  table:{
+    minWidth:1080
+  }
+};
  
 const customers=[
   {
@@ -30,9 +49,25 @@ const customers=[
   
 ]
 
-function App() {
+class App extends Component {
+  render(){
+
+  
+  const {classes}=this.props?this.props:null;
   return (
-    <div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+          <TableCell>번호</TableCell>
+          <TableCell>이미지</TableCell>
+          <TableCell>이름</TableCell>
+          <TableCell>생년월일</TableCell>
+          <TableCell>성별</TableCell>
+          <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
       {customers.map(c => {
         return (
           <Customer
@@ -47,8 +82,13 @@ function App() {
         );
       })
     } 
-    </div>
+      
+      </TableBody>
+      </Table>
+      
+    </Paper>
   );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
